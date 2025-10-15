05a8435a9e894da1a1ab3b1f3a5be86cb2858ea3
Lê Quang Ánh <superanhziggy@gmail.com>
2025-10-15 12:36:34 +0700

    feat(cv):add notebook)

--- careermate/urls.py
+++ careermate/urls.py
@@ -4,6 +4,7 @@ from apps.roadmap_agent.views import TestView
 from home.swagger import schema_view
 
 urlpatterns = [
+    path('api/v1/', include('apps.cv_analysis_agent.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/test/', TestView.as_view(), name='test'),
 ]

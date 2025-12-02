980917fea14b99de27f838ab6d39ff2d0dd8971e
chinhnd23 <chinhnd23@fpt.com>
2025-12-02 11:48:51 +0700

    fix(settings): add STATIC_ROOT for collectstatic command

--- Careermate/settings.py
+++ Careermate/settings.py
@@ -111,6 +111,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = []
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Celery Configuration
